ae466b4d4fcce1f6cba0ee60ff14e4d6a244e556
Viktor Hronec <zamr666@gmail.com>
2017-06-02 21:54:14 +0200

    On the go test in main

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-# main.py -- put your code here!
-
 from machine import SPI, Pin
 import pyb
 import upcd8544
 import bar
-
-import pyb
-import upcd8544
-import bar
+from text import TextWriter
 from keypad import Keypad
 
-
-SPI    = SPI(1)
-RST    = Pin('X4')
-CE     = Pin('X5')
-DC     = Pin('X7')
-LIGHT  = Pin('X3')
+SPI = SPI(1)
+RST = Pin('X4')
+CE = Pin('X5')
+DC = Pin('X7')
+LIGHT = Pin('X3')
 
 lcd = upcd8544.PCD8544(SPI, RST, CE, DC, LIGHT)
+writer = TextWriter(lcd)
 
 bar.bat_disp(lcd, level=3)
 pyb.delay(500)
@@ -25,8 +20,8 @@ bar.sig_disp(lcd, level=3)
 
 kp = Keypad(('Y5', 'Y6', 'Y7', 'Y8'), ('Y1', 'Y2', 'Y3', 'Y4'))
 while True:
-    data = kp.get()
-    if data:
-        bt = data[0] * data[1] * 16
-        print(bt)
-        lcd.data([bt])
+    character = kp.get()
+    if character:
+        print(character)
+        writer._write_raw(character)
+    pyb.delay(50)
